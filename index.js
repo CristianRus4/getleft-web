6d0ca3a3a474ec63380b9bc0b5c5f3ffe186b079
Cristian Rus <cristianrus4@gmail.com>
2026-05-22 18:00:15 +1200

    fff

--- index.js
+++ index.js
@@ -501,7 +501,7 @@
     const dotsEl = document.querySelector('.ba-phone-dots');
     if (!dotsEl) return;
 
-    const COLS = 8, ROWS = 17;
+    const COLS = 10, ROWS = 3;
     const total = COLS * ROWS;
     const dots = [];
 
